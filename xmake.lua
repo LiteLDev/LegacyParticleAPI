fa408bb497175feea0de2f96ae08f1a71c8232b9
ShrBox <shrbox@outlook.com>
2024-03-31 20:52:27 +0800

    chore: update tooth.json

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,7 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-add_requires("levilamina 0.10.0")
+add_requires("levilamina 0.10.3")
 
 target("LegacyParticleAPI")
     add_cxflags(
